700992f5e5ef42c86e60c702d018e815f5f79c83
3pc-gitkraken05 <gitkraken05@3pc.de>
2021-05-13 16:15:23 +0200

    fix(cart): fix cart item image

--- gatsby-shopify/src/components/Cart/LineItem/index.js
+++ gatsby-shopify/src/components/Cart/LineItem/index.js
@@ -11,15 +11,8 @@ const LineItem = props => {
     store: { client, checkout },
   } = useContext(StoreContext)
 
-  console.log(item.variant.image.src)
   const variantImage = item.variant.image ? (
-    <StaticImage
-      src={item.variant.image.src}
-      alt={`${item.title} product shot`}
-      placeholder="blurred"
-      width={200}
-      height={200}
-    />
+    <img src={item.variant.image.src} alt={`${item.title} product shot`} />
   ) : null
 
   const selectedOptions = item.variant.selectedOptions
@@ -31,11 +24,9 @@ const LineItem = props => {
   const handleRemove = () => {
     removeLineItem(client, checkout.id, item.id)
   }
-  console.log('variantImage :>> ', variantImage)
   return (
     <Wrapper>
       <Link to={`/product/${item.variant.product.handle}/`}>
-        popo
         {variantImage}
       </Link>
       <p>
