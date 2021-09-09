4d62f75c30443971afeb894c394fb748e98810f2
Jessica Schäfer <jschaefer@3pc.de>
2021-09-09 22:20:35 +0200

    feat(Checkout): re-style checkout

--- gatsby-shopify/src/components/Checkout.js
+++ gatsby-shopify/src/components/Checkout.js
@@ -23,7 +23,7 @@ const CheckoutComponent = styled.div`
     transform: translateX(0%);
   } 
 
-  @media ${breakpoint.desktop} { 
+  @media ${breakpoint.desktop} {
     width: 50%;
   }
 
@@ -36,12 +36,49 @@ const CheckoutComponent = styled.div`
   }
 `;
 
+const CheckboxHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 15px;
+  padding-left: 50px;
+  padding-right: 150px;
+  padding-bottom: 15px;
+  border: 1px solid black;
+
+  @media ${breakpoint.desktop} {
+    padding-right: 100px; 
+  }
+
+  @media ${breakpoint.tablet} { 
+    padding-right: 80px;
+  }
+
+  @media ${breakpoint.mobile} { 
+    padding-right: 30px;
+    padding-left: 30px;
+  }
+`;
 
 // CHECKOUT INNER
 
 const CheckoutInner = styled.div`
-  display: inline-block;
-  margin-left: 50px;
+  margin-top: 25%;
+  margin-bottom: 5%;
+  padding-right: 150px; 
+  padding-left: 50px; 
+
+  @media ${breakpoint.desktop} {
+    padding-right: 100px; 
+  }
+
+  @media ${breakpoint.tablet} { 
+    padding-right: 80px;
+  }
+
+  @media ${breakpoint.mobile} { 
+    padding-left: 30px;
+    padding-right: 30px;
+  }
 `
 
 const CheckoutButton = styled.button`
@@ -88,13 +125,6 @@ const CheckoutLink = styled.button`
   display: block;
   margin-top: 15px;
   align-self: flex-start;
-`
-
-// CHECKOUT TITLE
-
-const CheckoutTitle = styled.h2`
-  margin-top: 25%;
-  margin-bottom: 5%;
 `
 
 
@@ -153,53 +183,50 @@ const Checkout = ({isOpen, handleCheckoutClose}) => {
 
   return (
     <CheckoutComponent className={`checkout ${isOpen ? 'is-active' : ''}`}>
-      
-      {/* CHECKOUT INNER */}
-      <CheckoutInner>
-
         {/* CHECKOUT CLOSE */}
-        <CheckoutClose onClick={() => handleCheckoutClose()}>
-          <svg
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg" 
-            width="10" 
-            height="10" 
-            viewBox="0 0 24 24">
-            <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/>
-          </svg>
-          <span className="sr-only">close</span>
-        </CheckoutClose>
-        {lineItems.length ? <>
-        <h2>Your Order</h2>
+        <CheckboxHeader>
+          <h2>Your Order</h2>
+          <CheckoutClose onClick={() => handleCheckoutClose()}>
+            <svg
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg" 
+              width="10" 
+              height="10" 
+              viewBox="0 0 24 24">
+              <path d="M23.954 21.03l-9.184-9.095 9.092-9.174-2.832-2.807-9.09 9.179-9.176-9.088-2.81 2.81 9.186 9.105-9.095 9.184 2.81 2.81 9.112-9.192 9.18 9.1z"/>
+            </svg>
+            <span className="sr-only">close</span>
+          </CheckoutClose>
+        </CheckboxHeader>
 
-        {/* CHECKOUT LIST */}
-        <CheckoutList>
-          {lineItems}
-        </CheckoutList>
+        {/* CHECKOUT INNER */}
+        <CheckoutInner>
+          {lineItems.length ? <>
+          {/* CHECKOUT LIST */}
+          <CheckoutList>
+            {lineItems}
+          </CheckoutList>
 
-        <Link to="/shipping/" className="link-hover">
-          <span>Free Shipping within 30 - 60 days</span>
-        </Link>
+          <Link to="/shipping/" className="link-hover">
+            <span>Free Shipping within 30 - 60 days</span>
+          </Link>
 
-        {/* CHECKOUT BUTTON */}
-        <CheckoutButton 
-          onClick={handleCheckout}
-          disabled={checkout.lineItems.length === 0}
-          >
-          <CheckoutPrice data-hover={`Check out — € ${checkout.totalPrice}`}>Check out — € {checkout.totalPrice}</CheckoutPrice>
-        </CheckoutButton>
-        </> : <>
+          {/* CHECKOUT BUTTON */}
+          <CheckoutButton 
+            onClick={handleCheckout}
+            disabled={checkout.lineItems.length === 0}
+            >
+            <CheckoutPrice data-hover={`Check out — € ${checkout.totalPrice}`}>Check out — € {checkout.totalPrice}</CheckoutPrice>
+          </CheckoutButton>
+          </> : <>
 
-        {/* CHECKOUT TITLE */}
-        <CheckoutTitle>Your Order</CheckoutTitle>
-
-        <p>Looks like you haven’t added anthing to your order yet.</p>
-        
-        {/* CHECKOUT LINK */}
-        <CheckoutLink className="link-hover" onClick={() => handleBackClick()}>
-          <span>Shop all</span>
-        </CheckoutLink> </>}
-      </CheckoutInner>
+          <p>Looks like you haven’t added anthing to your order yet.</p>
+          
+          {/* CHECKOUT LINK */}
+          <CheckoutLink className="link-hover" onClick={() => handleBackClick()}>
+            <span>Shop all</span>
+          </CheckoutLink> </>}
+        </CheckoutInner>
     </CheckoutComponent>
   )
 }
